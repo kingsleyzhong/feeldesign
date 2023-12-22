7af04cdea375ccdbaaca27ecac0c46119ab71f2a
Kingsley Zhong <kingsleyzhonghome@gmail.com>
2023-12-21 22:34:23 -0700

    added google tag + hotjar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -76,6 +76,29 @@ export default function App({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
+
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-X5BM7G1QJL" />
+        <Script>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-X5BM7G1QJL');
+        `}
+        </Script>
+
+        <Script>
+          {`
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3803659,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `}
+        </Script>
       </Head>
       <SessionProvider session={pageProps.session}>
         <Component {...pageProps} />
